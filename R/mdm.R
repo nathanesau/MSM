7c0e921582ebdef4f6eb56bbb042e85224c32190
Nathan Esau <nathanesau1@gmail.com>
2015-09-26 18:49:01 -0700

    added reserve

--- R/mdm.R
+++ R/mdm.R
@@ -63,6 +63,42 @@ Qxt.diag <- function(Qxt.fns, zero_diag=FALSE, joint=FALSE) {
     Qxt.fns
 }
 
+#' @title Non-diagonal of transition matrix
+#' @description Sets functions for the non-diagonal of the transition matrix, Q, to 0
+#' @param Qxt.fns A matrix of transition functions 
+#' @param zero_diag Indicate whether the diagonal should be all functions which return 0
+#' @param joint Indicate whether the tranisition matrix is for joint lives (i.e. functions of x and y)
+#' @return A matrix of transition functions with updated non-diagonal entries
+#' @details Should be called after Qxt.diag has been used on Qxt.fns. Used by tV() function
+#' @export 
+Qxt.nondiag <- function(Qxt.fns, joint=FALSE) {
+    N <- length(Qxt.fns[1,])
+    tmp <- Qxt.fns[[1]] 
+    
+    for(i in rep(seq(2:(N+1)), N-1) + rep(0:(N-2),each=N)*(N+1) + 1) {
+        if(joint) {
+            Qxt.fns[[i]] <- function(t=0,x=0,y=0,muxt) 0 * t
+        } else {
+            Qxt.fns[[i]] <- function(t=0,x=0,muxt) 0 * t
+        }
+    }
+    Qxt.fns
+}
+
+#' @title Zero function matrix
+#' @description Creates a matrix of functions. Each element is a function of t, which returns 0 * t
+#' @param nrow The number of rows for the matrix
+#' @param ncol The number of columns for the matrix
+#' @return A matrix of functions (of t) which each return 0 
+#' @export 
+zfm <- function(nrow=1, ncol=1) {
+    zfm.matrix <- list()
+    for(i in (1:(nrow*ncol))) {
+        zfm.matrix[[i]] <- function(t) 0 * t
+    }
+    matrix(zfm.matrix, nrow=nrow, ncol=ncol)
+}
+
 #' @title Transition Probabilities
 #' @description Probability of transitioning from one state to another within t years. Approximation based on the Kolmogorov forward equations.
 #' @param object An object of class msm
@@ -158,7 +194,7 @@ setMethod(f = "show", signature = "msm",
 #' @param y The age of the second individual if the model is joint
 #' @return A matrix of expected values for annuities 
 #' @export 
-ax <- function(object, n=10, x=0, h=1/12, delta=log(1.05), dis=0,
+annx <- function(object, n=10, x=0, h=1/12, delta=log(1.05), dis=0,
                joint=FALSE, y=0) {
 
     tPx.all <- tPx(object, t = n, x, h, all = TRUE, joint, y)
@@ -199,7 +235,7 @@ ax <- function(object, n=10, x=0, h=1/12, delta=log(1.05), dis=0,
 #' @export 
 Ax <- function(object, n=10, x=0, h=1/12, delta=log(1.05),
                joint=FALSE, y=0) {
-    # we need a matrix of functions Qx+t' which is Qx+t with a zero diagonal
+    # we need a matrix of fuecho $DESKTOP_SESSIONnctions Qx+t' which is Qx+t with a zero diagonal
     tPx.all <- tPx(object, t = n, x, h, all = TRUE, joint, y)
     Qxt.prime <- Qxt.diag(object@Qxt, zero_diag = T)
     N <- length(Qxt.prime[1,])
@@ -215,7 +251,6 @@ Ax <- function(object, n=10, x=0, h=1/12, delta=log(1.05),
     }
     tPx.all[[1]] <- tPx.all[[1]] %*% Qxt.vals
     
-    it <- n/h
     for(i in 1:it) {
         t <- i*h
         
@@ -242,4 +277,77 @@ Ax <- function(object, n=10, x=0, h=1/12, delta=log(1.05),
     fa = tPx.all[[1]]
     fb = tPx.all[[it+1]]
     h/3 * (fa + fb + 4*Reduce("+", y1) + 2*Reduce("+", y2))
+}
+
+#' @title Reserve at time-t (matrix form)
+#' @description Calculates the value of time-t Reserves by using Euler's method 
+#'  with Thiele's differential equation
+#' @param object An object of class msm 
+#' @param t The time to calculate the reserve at
+#' @param x The age of the policyholder
+#' @param h The time step to use
+#' @param V0 The initial reserve values (a forward recursion is used)
+#' @param B A matrix of benefit payment functions to denote the rate of payment of net benefit at time t 
+#'  while the policyholder is in state i. B should be a vector of length N where N is the number of states (i = 0... n)
+#' @param S A matrix of lump sum benefit functions to denote the rate of lump sum benefit payments, payable at time t,
+#'  on transition from state i to state j.
+#' @param delta The force of interest (assumed constant)
+#' @param all indicate whether all the tV values should be returned (by default only the last tV is returned)
+#' @return The t-time reserve for each state
+#' @export 
+tV <- function(object, t=0, x=0, h=1/12, V0=NA, B=NA, S=NA, delta=log(1.05),
+               all=FALSE) {
+    Qxt.prime <- Qxt.nondiag(object@Qxt)
+    N <- length(Qxt.prime[1,])
+
+    # initial value of reserve
+    if(is.na(V0[1])) tV.value <- matrix(0,nrow=N)
+    else tV.value <- V0
+    
+    # benefit payment functions
+    if(is.na(B[1])) B <- zfm(nrow=N, ncol=1)
+    else B <- B
+
+    # lump sum benefit functions
+    S <- if(is.na(S[1])) S <- zfm(nrow=N,ncol=N)
+    else S <- S
+    
+    it <- t/h
+    tV.all <- vector("list", it + 1) # store all the reserves
+    
+    # i.e. if t = 1, h=1/12, then i = 12 is the number of iterations
+    it <- t/h 
+    for(i in 1:it) {
+        tV.all[[i]] <- tV.value
+        
+        # evaluate transition intensities
+        Qxt.vals <- matrix(unlist(
+            lapply(Qxt.prime, function(f) f(t=(i-1)*h,x))), 
+            N, N, byrow=TRUE)
+        
+        # evaluate benefit payment functions
+        B.vals <- matrix(unlist(
+            lapply(B, function(f) f(t=(i-1)*h))),
+            N, 1)
+        
+        # evaluate lump sum benefit functions
+        S.vals <- matrix(unlist(
+            lapply(S, function(f) f(t=(i-1)*h))),
+            N, N, byrow=TRUE)
+    
+        # V*
+        V.vals <- matrix(rep(tV.value,N), N, byrow=TRUE)
+        
+        # tPx * = St + V* + t(V*)
+        tPx.vals <- S.vals + V.vals + t(V.vals)
+        
+        tV.value <- tV.value + h * (delta * tV.value - B.vals - apply(Qxt.vals %*% tPx.vals, 1, sum))
+    }
+    
+    tV.all[[it+1]] <- tV.value
+    
+    if(all) 
+        tV.all
+    else 
+        tail(tV.all,1)[[1]]
 }
